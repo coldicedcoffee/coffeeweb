71fa617204365c7ae73d7841ebc9c6b6094b4128
Param Pabari <163516175+coldicedcoffee@users.noreply.github.com>
2025-11-29 13:49:29 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/coffeeweb/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
